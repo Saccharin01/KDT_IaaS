4b7e9b85c6c603d75693eff535a732f7fb532f44
Saccharin01 <chousik01@gmail.com>
2024-06-26 02:21:43 +0900

    newItem swallowDeep
    클래스 룸의 예제로서 얕은 복사와 깊은 복사에 대한 예시
    
    얕은 복사는 원본값을 참조함. 새로운 대상을 만들어내는 것이 아니라 원본을 참조하는 것.
    깊은 복사는 원본값을 바탕으로 새로운 대상을 만들어 냄.

---
+++ classRoom/swallowDeep.js
@@ -0,0 +1,27 @@
+const original = [
+  { name: "조우식", age: 30 },
+  { name: "사카린", age: 4 },
+];
+
+const swallow = original.map((ele) => {
+  return ele;
+});
+const deep = original.map((ele) => {
+  let result = {
+    name: ele.name,
+    age: ele.age,
+  };
+  return result;
+});
+
+
+console.log("원본 배열 : ", original)
+console.log("swallowCopy : ", swallow)
+console.log("deepCody : ", deep)
+
+original[0].name = "Saccharin"
+
+console.log("변경 후의 결과 값")
+console.log("원본 배열 : ", original)
+console.log("swallowCopy : ", swallow)
+console.log("deepCody : ", deep)
