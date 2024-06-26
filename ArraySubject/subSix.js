a00a7100ac7e325502b9ed90b89d170dd11d750f
Saccharin01 <chousik01@gmail.com>
2024-06-26 14:32:50 +0900

    subSix.js added

---
+++ ArraySubject/subSix.js
@@ -0,0 +1,9 @@
+const items = [
+  {name : "콜라", stock :5},
+  {name : "사이다", stock :0},
+  {name : "물", stock :10},
+  {name : "쥬스", stock :2}
+]
+
+// todo 모든 상품의 이름을 배열로 생성하세요
+const itemNames = items.someMethod((elemenet)=>{return elemenet.name})
