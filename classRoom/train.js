200398f1f4cc99acc2a62487d4bc2fe9ff0b0705
Saccharin01 <chousik01@gmail.com>
2024-06-26 10:52:43 +0900

    middle save

--- classRoom/train.js
+++ classRoom/train.js
@@ -1,5 +1,7 @@
 const a = 1;
 const b = 1;
-console.log(a === b)
-const c = [1];
+console.log(a === b);
+const c = [1]; // ? 변수 C에 인덱스 0번째는
 const d = [1];
+console.log(c[0] === d[0]); // ? 인덱스에 해당하는 value가 같은거야???
+console.log(c === d); // ? 참조하는 주소가 다르기 때문에 false 가 나오는거 아냐?
