caca04072dbcaf255d92ae9119b2864a2196e6b8
Saccharin01 <chousik01@gmail.com>
2024-06-26 17:55:29 +0900

    subSeven.js added

---
+++ ArraySubject/subSeven.js
@@ -0,0 +1,8 @@
+const items = [
+  { name: "콜라", price: 5 },
+  { name: "사이다", price: 0 },
+  { name: "물", price: 10 },
+  { name: "쥬스", price: 2 },
+];
+
+// todo 가격이 1000원 이상인 상품만 나열하도록 수정하세요 분할정복 알고리즘 정렬 알고리즘
